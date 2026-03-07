3e3e76b043a94fbd9b407b399bd59a6ca2968893
Zidane K <84156972+danezi@users.noreply.github.com>
2026-03-07 16:03:47 +0100

    feat:ScrollView in the  Weclcome screen

--- first-rn-component/App.js
+++ first-rn-component/App.js
@@ -1,4 +1,3 @@
-import * as React from 'react'; 
 import { View } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
@@ -9,10 +8,11 @@ export default function App() {
   return (
     <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#495E57' }}>
       <LittleLemonHeader />
+      //flex:1 must be added to the view that contains the scrollview, otherwise the scrollview will not work
       <View style={{ flex: 1 }}>
         <WelcomeScreen />
       </View>
       <LittleLemonFooter />
     </View>
-  );
+  );  
 }
