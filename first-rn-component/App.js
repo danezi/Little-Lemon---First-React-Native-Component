7d3d7ca52582ad84cba58d2430839f00a06a7784
Zidane K <84156972+danezi@users.noreply.github.com>
2026-03-07 15:04:01 +0100

    feat: Weclcome screeb with message

--- first-rn-component/App.js
+++ first-rn-component/App.js
@@ -3,12 +3,13 @@ import { View } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './components/WelcomeScreen';
 
 export default function App() {
   return (
     <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#495E57' }}>
       <LittleLemonHeader />
-      <View style={{ flex: 1 }} />
+      <WelcomeScreen />
       <LittleLemonFooter />
     </View>
   );
