aef2808511c181e0d596e33130196913f1269498
Zidane K <84156972+danezi@users.noreply.github.com>
2026-03-09 15:42:01 +0100

    feat: Render a large list using FlatList (for FlatList -> renderitems and data must be set)

--- first-rn-component/App.js
+++ first-rn-component/App.js
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './components/WelcomeScreen';
+import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LittleLemonHeader />
-      <View style={{ flex: 1 }}>
-        <WelcomeScreen />
+        <LittleLemonHeader />
+        <MenuItems />
+      <View style={styles.footerContainer}>
+        <LittleLemonFooter />
       </View>
-      <LittleLemonFooter />
     </View>
   );
 }
