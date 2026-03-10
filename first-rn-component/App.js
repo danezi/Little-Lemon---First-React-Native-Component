071bf629f78f06637cbbba44be181537a0adce89
Zidane K <84156972+danezi@users.noreply.github.com>
2026-03-10 13:03:25 +0100

    loginscreen and WelcomeScreen(modified) faetures: placeholder TextInput TextInput secureTextEntry

--- first-rn-component/App.js
+++ first-rn-component/App.js
@@ -3,17 +3,19 @@ import { View, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import MenuItems from './components/MenuItems';
+import LoginScreen from './components/LoginScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
+      <View style={styles.container}>
         <LittleLemonHeader />
-        <MenuItems />
+        <LoginScreen />
+      </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
       </View>
-    </View>
+    </>
   );
 }
 
@@ -24,4 +26,3 @@ const styles = StyleSheet.create({
   },
   footerContainer: { backgroundColor: '#333333' },
 });
-
