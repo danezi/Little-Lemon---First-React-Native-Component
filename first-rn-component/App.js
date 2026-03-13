61bc76a6c97039fd5a35690767bf8c79cdc5ad11
Zidane K <84156972+danezi@users.noreply.github.com>
2026-03-13 14:34:50 +0100

    Imgae: Welcomescreen.js with image little lemon logo

--- first-rn-component/App.js
+++ first-rn-component/App.js
@@ -4,13 +4,15 @@ import { View, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import LoginScreen from './components/LoginScreen';
+import WelcomeScreen from './components/WelcomeScreen';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <LoginScreen />
+        {/* <LoginScreen /> */}
+        <WelcomeScreen />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
